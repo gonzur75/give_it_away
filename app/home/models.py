abe235186b35afb36cb9d470a7bea7fd05901ae3
Marcin <gonzur75@gmail.com>
2022-04-23 11:01:56 +0200

    add institution model

--- app/home/models.py
+++ app/home/models.py
@@ -1,8 +1,32 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Category(models.Model):
     name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+
+class Institution(models.Model):
+    FOUNDATION = 'FU'
+    ORGANISATION = 'OP'
+    COLLECTION = 'ZL'
+    INSTITUTION_TYPE_CHOICES = [
+        ('FU', _('fundacja')),
+        ('OP', _('organizacja pozarządowa')),
+        ('ZL', _('zbiórka lokalna')),
+    ]
+
+    name = models.CharField(max_length=64, unique=True)
+    description = models.CharField(max_length=255)
+    type = models.CharField(
+        max_length=2,
+        choices=INSTITUTION_TYPE_CHOICES,
+        default=FOUNDATION,
+    )
+    categories = models.ManyToManyField('Category')
 
     def __str__(self):
         return self.name
--- app/home/tests/test_view.py
+++ app/home/tests/test_view.py
@@ -1,8 +1,4 @@
-# Create your tests here.
-import pytest
 from django.urls import reverse_lazy
-
-from home.models import Category
 
 
 def test_landing_page_view(client):
@@ -13,12 +9,3 @@ def test_landing_page_view(client):
 def test_add_donation_view(client):
     response = client.get(reverse_lazy('home:add_donation'))
     assert response.status_code == 200
-
-
-@pytest.mark.django_db
-def test_category_model_object_create():
-    test_category = Category.objects.create(
-        name='Test name'
-    )
-
-    assert Category.objects.last() == test_category
