fb2a7a02edbc3f1c2758e906f925877e2ab5faf9
Marcin <gonzur75@gmail.com>
2022-04-27 21:07:03 +0200

    add login redirection url in settings

--- app/config/settings.py
+++ app/config/settings.py
@@ -142,3 +142,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+LOGIN_REDIRECT_URL = 'home:landing_page'
